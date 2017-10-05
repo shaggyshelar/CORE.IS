5a74d449919e33dc5b4e3f6ae3c2704110fa1fff
Sagar Shelar <shaggy.shelar@gmail.com>
2017-10-05 15:30:54 +0530

    App in running mode

--- src/CORE.IS.EntityFrameworkCore/Migrations/20171005095404_Initial.Designer.cs
+++ src/CORE.IS.EntityFrameworkCore/Migrations/20171005095404_Initial.Designer.cs
@@ -15,9 +15,10 @@ using System;
 namespace CORE.IS.Migrations
 {
     [DbContext(typeof(ISDbContext))]
-    partial class ISDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171005095404_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
